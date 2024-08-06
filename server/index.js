43603fa3a374c8c17bdaa087990206105932a015
uisha <joshua_emmanuel_g_tipon@dlsu.edu.ph>
2024-08-07 02:51:40 +0800

    deleted api/employee from API due to being unused and unecessary

--- server/index.js
+++ server/index.js
@@ -107,17 +107,6 @@ async function startApp() {
         res.json({ success: false, message: "search error" });
       }
     });
-  
-    app.post('/api/employees', async (req, res) => {
-      try {
-        const employees = await db.collection('users').find({}).toArray();
-        // console.log("Employees fetched from DB:", employees); // Debugging line
-        res.json({ success: true, message: "Employees fetched successfully", result: employees });
-      } catch (error) {
-        console.error("Error fetching employees:", error);
-        res.json({ success: false, message: "Error fetching employees" });
-      }
-    });
 
   app.post('/api/delete/:modelName/:id', verifyToken, async (req, res) => {
       const { modelName, id } = req.params;
