a54f0f839b392a23d6c59411fd7a953dc1fd6b85
uisha <joshua_emmanuel_g_tipon@dlsu.edu.ph>
2024-08-07 02:06:26 +0800

    protected /registration route

--- src/App.js
+++ src/App.js
@@ -51,7 +51,11 @@ const router = createBrowserRouter([
   },
   {
     path: "/registration",
-    element: <Registration/>,
+    element: (
+      <ProtectedRoute allowedRoles={['admin']}>
+        <Registration />,
+      </ProtectedRoute>
+    )
   },
 ]);
 
